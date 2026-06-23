ea042b57b6ef0e5b4b204d2c28652ad3d4ecf225
Stella Laurenzo <stellaraccident@gmail.com>
2026-06-23 15:40:35 -0700

    Reset for loom kernel integration

--- ggml/src/ggml-hrx/kernels/flash_attn_ext_f32_f16_prefill_direct.hip.cpp
+++
@@ -1,7 +0,0 @@
-#if !defined(GGML_HRX_FORCE_GENERIC_FA_DIRECT) &&                                                    \
-    (defined(__gfx1100__) || defined(__gfx1101__) || defined(__gfx1102__) || defined(__gfx1103__) || \
-     defined(__gfx1150__) || defined(__gfx1151__))
-#    include "flash_attn_ext_f32_f16_prefill_direct_gfx11.inc"
-#else
-#include "flash_attn_ext_f32_f16_prefill_direct_generic.inc"
-#endif
